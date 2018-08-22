840e3ed89812d44ae68c81bf47c02fa09286f3a6
Admin <brookspatola@gmail.com>
2018-08-22 14:48:33 -0400

    added video to homepage

--- page-templates/custom.php
+++ page-templates/custom.php
@@ -13,31 +13,40 @@ get_header();
 
 <!-- **************************HTML GOES HERE ************-->
 
- <!-- JUMBOTRON -->
- <div class="container">
- <div class="jumbotron text-center">
-      <h1 class="display-4">Welcome To My Website!</h1>
-      <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum necessitatibus perspiciatis cum vitae, modi hic?</p>
-      <hr>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, quibusdam.</p>
-      <a class="btn btn-success" href="#" role="button">Read More</a>
-    </div>
+  <!-- Video for fullscreen intro with fallback video formats for different browsers -->
+  <div class="container">
+  <section class= "introVideo">
+  <div>
+      <!-- muted for autoplay to work in Chrome -->
+      <video muted autoplay="true" loop="true">
+          <source src="http://127.0.0.1/wordpress/wp-content/uploads/2018/08/MOMU-SHORT.mp4" type="video/mp4">
+          <source src="video/holoVid.webm" type="video/webm">
+          <source src="video/holoVid.ogv" type="video/ogg">
+          <!-- Provide user message if their browser doesn't support html5 video -->
+          your browser doesn't support HTML5 video.
+      </video>
+  </div>
+  </section>
+
+
+
+<br><br>
 
 
  <!-- owl sliding gallery -->
-<div class="clients owl-carousel owl-theme">
+<div class="clients owl-carousel">
     <div class="thumbnail">
-        <img src="http://127.0.0.1/wordpress/wp-content/uploads/2018/08/holoNike.png" alt="nike">
+        <img src ="http://127.0.0.1/wordpress/wp-content/uploads/2018/08/holoNike.png" alt="nike">
     </div>
 
-        <div class="thumbnail">
+      <!--   <div class="thumbnail">
             <img src="img/holoAns.png" alt="Nature">
-        </div>
+        </div> -->
        
 </div>
 
 </div>
-
+<!--end container -->
 
  <!-- footer call -->
 <?
