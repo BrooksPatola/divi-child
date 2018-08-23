73f9fc8ce4b4a6d640d8c5114dc879fef4ceeecb
Admin <brookspatola@gmail.com>
2018-08-23 11:03:51 -0400

    updated enque functionality

--- functions.php
+++ functions.php
@@ -17,14 +17,10 @@ function sk_enqueue_scripts() {
  
 
         // bootstrap style
-    wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
 
 	wp_enqueue_style( 'animate', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css' );
  
-
-    //bootstrap js
-    wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
-    
 	wp_enqueue_script( 'wow', 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js', array(), '', true );
  
 
@@ -40,7 +36,25 @@ function sk_wow_init_in_footer() {
 
 //* Add JavaScript before </body>
 function wow_init() { ?>
+
 	<script type="text/javascript">
 		new WOW().init();
-	</script>
+    </script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js " integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em "
+        crossorigin="anonymous "></script>
+
 <?php }
+
+
+/* 
+function PREFIX_remove_scripts() {
+    wp_dequeue_style( 'screen' );
+    wp_deregister_style( 'screen' );
+
+    wp_dequeue_script( 'site' );
+    wp_deregister_script( 'site' );
+
+    // Now register your styles and scripts here
+}
+add_action( 'wp_enqueue_scripts', 'PREFIX_remove_scripts', 20 ); */
